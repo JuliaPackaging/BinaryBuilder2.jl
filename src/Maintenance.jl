ad722201d4cdadb59b80fde5b8e34440b776c373
Elliot Saba <staticfloat@gmail.com>
2025-02-22 05:09:18 +0000

    Put maintenance commands in their own module
    Also improve `ccache` maintenance commands a bit to show more and do
    more.  We probably want to set up our Yggdrasil runners to run some
    maintenance commands periodically.

--- src/Maintenance.jl
+++ src/Maintenance.jl
@@ -1,5 +1,12 @@
+module Maintenance
+
 # This file will collect entrypoints for maintenance of a build system.
 using Ccache_jll
+import ..BinaryBuilder2: ccache_cache
+
+function ccache_env(cmd::Cmd)
+    return addenv(cmd, "CCACHE_DIR" => ccache_cache())
+end
 
 """
     recompress_ccache_cache(;compression_level::Int=19)
@@ -14,15 +21,21 @@ and the size of the `ccache`.  As a datapoint, recompressing a cache of 4.0GB,
 with 32 cores and at level 19, took ~3 minutes and squashed the cache down to a
 total of 3.4 GB compressed (12.3 GB uncompressed).
 """
-function recompress_ccache_cache(;compression_level::Int=9)
-    run(addenv(`$(ccache()) --recompress $(compression_level)`, "CCACHE_DIR" => ccache_cache()))
+function ccache_recompress_cache(;compression_level::Int=9)
+    run(ccache_env(`$(ccache()) --cleanup --recompress $(compression_level)`))
 end
 
-function clear_ccache_cache()
-    run(addenv(`$(ccache()) -C`, "CCACHE_DIR" => ccache_cache()))
+function ccache_clear_cache()
+    run(ccache_env(`$(ccache()) --clear`))
+end
+
+function ccache_show_stats()
+    run(ccache_env(`$(ccache()) --show-stats --show-compression --verbose`))
 end
 
 # TODO:
 #  - artifact LRU trimming?
 #  - Old universe deletion?
 #  - DepotCompactor.jl integration?
+
+end # module Maintenance
