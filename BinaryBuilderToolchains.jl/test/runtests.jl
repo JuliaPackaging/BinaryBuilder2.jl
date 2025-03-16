764729d8bbcbc8f12d4a1629eebf3182e034b0ba
Elliot Saba <staticfloat@gmail.com>
2025-03-16 21:16:05 +0000

    Toolchains: Clear out some environment variables during test

--- BinaryBuilderToolchains.jl/test/runtests.jl
+++ BinaryBuilderToolchains.jl/test/runtests.jl
@@ -1,5 +1,14 @@
 using Test, BinaryBuilderToolchains
 
+# Clear out environment variables that can cause problems
+for flagvar in ("C", "CPP", "CXX", "LD")
+    delete!(ENV, "$(flagvar)FLAGS")
+end
+for libvar in ("LD", "DYLD")
+    delete!(ENV, "$(libvar)_LIBRARY_PATH")
+    delete!(ENV, "$(libvar)_FALLBACK_LIBRARY_PATH")
+end
+
 include("common.jl")
 include("PkgUtilsTests.jl")
 include("WrapperUtilsTests.jl")
