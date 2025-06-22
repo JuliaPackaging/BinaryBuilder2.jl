12cbb4516292840826789e9f5795355d52f75a78
Elliot Saba <staticfloat@gmail.com>
2025-06-21 20:25:52 -0700

    Basic precompilation workload for BinaryBuilder2
    We can't do a full build as part of the precompilation workload because
    of `Pipe()` issues.  I have a question in to Jameson about this.

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,2 +1,26 @@
-Base.precompile(BuildConfig, (String, VersionNumber, Vector{AbstractSource}, String, AbstractPlatform))
-Base.precompile(SandboxConfig, (BuildConfig,))
+using PrecompileTools
+
+@setup_workload begin
+    @compile_workload begin
+        # Make this not dry_run once I can run with pipes at precompile time
+        meta = BuildMeta(;verbose=false)
+
+        # Warm up the `build_tarballs()` loop.  Because we're, complete with deploying C toolchains and whatnot.
+        # This could take a while the first time (due to actually needing to download some compiler
+        # shards and whatnot) but should be not _too_ slow as long as you've got some artifacts locally.
+        build_tarballs(;
+            src_name = "blank",
+            src_version = v"1.0.0",
+            sources = [],
+            script = "touch LICENSE.md",
+            platforms = [BBHostPlatform()],
+            meta,
+        )
+
+        # Warm up the `runshell` machinery
+        runshell(BBHostPlatform(); meta, shell=`/bin/true`, verbose=true)
+
+        # Clean up silently
+        cleanup(meta.universe; silent=true)
+    end
+end
